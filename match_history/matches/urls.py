1453a4031dd1d3dc14627db8c415973614b1c32c
Abdelmajid El-oualy <abdel-ou@c1r4p1.1337.ma>
2024-09-20 09:28:58 +0100

    matches

--- match_history/matches/urls.py
+++ match_history/matches/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.getData, name='index'),
+    path('user/<str:username>/',views.getData, name='index'),
     path('add/',views.postData, name='add'),
+   
 ]
--- match_history/matches/views.py
+++ match_history/matches/views.py
@@ -4,12 +4,15 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Match
 from .serializers import MatchSerializer
+from django.db.models import Q
 
 # Create your views here.
 
 @api_view(['GET'])
-def getData(request):
-    data = Match.objects.all()
+def getData(request,username):
+    print(username)
+    
+    data = Match.objects.filter(Q(player1=username) | Q(player2=username)).order_by('-id')
     serializer = MatchSerializer(data, many=True)
     return Response(serializer.data)
 
