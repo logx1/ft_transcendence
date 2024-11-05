cfd9b1b32a8860a07c2fd7de31f8bc20df45ce28
Abdelmajid El-oualy <abdel-ou@c1r4p1.1337.ma>
2024-11-05 20:46:15 +0100

    add micro serv

--- match_history/matches/views.py
+++ match_history/matches/views.py
@@ -5,13 +5,27 @@ from rest_framework.decorators import api_view
 from .models import Match
 from .serializers import MatchSerializer
 from django.db.models import Q
+import requests
 
 # Create your views here.
 
 @api_view(['GET'])
 def getData(request,username):
     print(username)
-    
+    token = request.COOKIES.get('access')
+    url = 'http://127.0.0.1:8001/api/user/'
+    #i whant to include the token in the header as cookie
+    cookies = {
+        'access': token
+    }
+
+    response = requests.get(url, cookies=cookies)
+
+
+    if response.status_code != 200:
+        return Response({'error': 'Invalid token'}, status=400)
+    # print(response.json())
+    print(response.json())
     data = Match.objects.filter(Q(player1=username) | Q(player2=username)).order_by('-id')
     serializer = MatchSerializer(data, many=True)
     return Response(serializer.data)
